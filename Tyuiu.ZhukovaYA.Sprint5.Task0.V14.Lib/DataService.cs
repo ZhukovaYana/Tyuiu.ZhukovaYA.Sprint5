0a9aa772b6fe0a6e4c8838f71d34c542f5796bbf
Yana <yana_zhukova2004@mail.ru>
2024-11-28 19:40:22 +0500

    Task0

---
+++ Tyuiu.ZhukovaYA.Sprint5.Task0.V14.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ZhukovaYA.Sprint5.Task0.V14.Lib
+{
+    public class DataService : ISprint5Task0V14
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            double res = ((4 * x) ^ 3) / ((x ^ 3) - 1);
+            File.WriteAllText(path, Math.Round(res, 3).ToString());
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.ZhukovaYA.Sprint5.Task0.V14/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.ZhukovaYA.Sprint5.Task0.V14.Lib;
+namespace Tyuiu.ZhukovaYA.Sprint5.Task0.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Жукова Яна Андреевна | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Запись данных в текстовый файл                                          *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Жукова Яна Андреевна | ИИПБ-24-1                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дано выражение вычислить его значение при x = 3, результат сохранить в те*");
+            Console.WriteLine("*екстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до трёх*");
+            Console.WriteLine("*х знаков после запятой.                                                  *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            int x = 3;
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+
+            Console.ReadKey();
+        }
+    }
+}
