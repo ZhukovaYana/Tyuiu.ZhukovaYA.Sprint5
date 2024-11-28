be2cea4e5282d63fffcf4bbf2302e5d3ef6e5ff6
Yana <yana_zhukova2004@mail.ru>
2024-11-28 20:41:12 +0500

    Task3

--- Tyuiu.ZhukovaYA.Sprint5.Task3.V14.Lib/DataService.cs
+++ Tyuiu.ZhukovaYA.Sprint5.Task3.V14.Lib/DataService.cs
@@ -7,8 +7,8 @@ namespace Tyuiu.ZhukovaYA.Sprint5.Task3.V14.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
-            double res = (4 * Math.Pow(x, 3)) / (Math.Pow(x, 3) - 1);
-            File.WriteAllText(path, Math.Round(res, 3).ToString());
+            double res = (double)(4 * Math.Pow(x, 3)) / (Math.Pow(x, 3) - 1);
+            File.WriteAllText(path, Math.Round((double)res, 3).ToString());
             return path;
         }
     }
