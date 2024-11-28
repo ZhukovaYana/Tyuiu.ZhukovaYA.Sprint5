3f65be7e0aca9a5c75d84a68c04e4bf38a0c4629
Yana <yana_zhukova2004@mail.ru>
2024-11-28 19:54:49 +0500

    Task1

---
+++ Tyuiu.ZhukovaYA.Sprint5.Task1.V14.Lib/DataService.cs
@@ -0,0 +1,46 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ZhukovaYA.Sprint5.Task1.V14.Lib
+{
+    public class DataService : ISprint5Task1V14
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool FileExists = fileInfo.Exists;
+
+            if (FileExists)
+            {
+                File.Delete(path);
+            }
+            double y;
+            string strY;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0)
+                {
+                    File.AppendAllText(path, "0");
+                    continue;
+                }
+                y = (Math.Sin(x)/(x+1.7)) - Math.Cos(x)*4*x-6;
+                y = Math.Round(y, 2);
+
+                strY = Convert.ToString(y);
+
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, strY + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, strY);
+                }
+
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.ZhukovaYA.Sprint5.Task1.V14/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.ZhukovaYA.Sprint5.Task1.V14.Lib;
+namespace Tyuiu.ZhukovaYA.Sprint5.Task1.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Жукова Яна Андреевна | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Запись набора данных в текстовый файл                                   *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Жукова Яна Андреевна | ИИПБ-24-1                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("startValue: " + startValue);
+            Console.WriteLine("stopValue: " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл создан: " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
