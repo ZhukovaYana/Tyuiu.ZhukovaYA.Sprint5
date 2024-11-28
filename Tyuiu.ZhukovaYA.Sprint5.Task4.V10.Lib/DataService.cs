31c978fb79fe84226576446e505e938f3d0dd384
Yana <yana_zhukova2004@mail.ru>
2024-11-28 21:37:28 +0500

    Task4

--- Tyuiu.ZhukovaYA.Sprint5.Task4.V10.Lib/DataService.cs
+++ Tyuiu.ZhukovaYA.Sprint5.Task4.V10.Lib/DataService.cs
@@ -7,6 +7,7 @@ namespace Tyuiu.ZhukovaYA.Sprint5.Task4.V10.Lib
         public double LoadFromDataFile(string path)
         {
             string strx = File.ReadAllText(path);
+            strx = strx.Replace('.', ',');
             double x = Convert.ToDouble(strx);
             double res = Math.Round(Math.Pow(x,3)*1.2*x+2, 3);
             return res;
