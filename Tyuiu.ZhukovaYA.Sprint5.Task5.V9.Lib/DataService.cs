e90353a0c482e82afa01fd1592b347992da68f76
Yana <yana_zhukova2004@mail.ru>
2024-11-29 19:44:43 +0500

    Task5

--- Tyuiu.ZhukovaYA.Sprint5.Task5.V9.Lib/DataService.cs
+++ Tyuiu.ZhukovaYA.Sprint5.Task5.V9.Lib/DataService.cs
@@ -9,18 +9,17 @@ namespace Tyuiu.ZhukovaYA.Sprint5.Task5.V9.Lib
             double res = double.MinValue;
             using (StreamReader reader = new StreamReader(path))
             {
-                string line;
-                while ((line = reader.ReadLine()) != null)
+                string[] line = reader.ReadToEnd().Split(' ');
+                for (int i = 0; i < line.Length; i++)
                 {
-                    res = Convert.ToDouble(line) % 1;
-                    if (double.TryParse(line, out double number))
+                    res = Convert.ToDouble(line[i].Replace('.', ','));
+                    if (double.TryParse(line[i], out double number))
                     {
                         if (number > res)
                         {
                             res = number;
                         }
                     }
-
 
                 }
                 return res;
