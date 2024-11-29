68574b21e4b6e3c7552bdfaf0bbf73fa5ebfcb80
Yana <yana_zhukova2004@mail.ru>
2024-11-29 18:44:45 +0500

    Task5

---
+++ Tyuiu.ZhukovaYA.Sprint5.Task5.V9.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ZhukovaYA.Sprint5.Task5.V9.Lib
+{
+    public class DataService : ISprint5Task5V9
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (Convert.ToInt32(line) > res)
+                    {
+                        res = Convert.ToInt32(line);
+                    }
+                }
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.ZhukovaYA.Sprint5.Task5.V9.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.ZhukovaYA.Sprint5.Task5.V9.Lib;
+
+namespace Tyuiu.ZhukovaYA.Sprint5.Task5.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"С:/DataSprint5/InPutDataFileTask5V9.txt";
+            FileInfo fileInfo = new FileInfo(path);
+
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
