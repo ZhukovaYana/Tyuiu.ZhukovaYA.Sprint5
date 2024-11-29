4d7cec36015899f01fd72a026d4fb2d1c2ffc5bb
Yana <yana_zhukova2004@mail.ru>
2024-11-29 18:46:14 +0500

    Task5

--- Tyuiu.ZhukovaYA.Sprint5.Task5.V9.Lib/DataService.cs
+++ Tyuiu.ZhukovaYA.Sprint5.Task5.V9.Lib/DataService.cs
@@ -6,6 +6,7 @@ namespace Tyuiu.ZhukovaYA.Sprint5.Task5.V9.Lib
     {
         public double LoadFromDataFile(string path)
         {
+            path = path.Replace('.', ',');
             double res = 0;
             using (StreamReader reader = new StreamReader(path))
             {
